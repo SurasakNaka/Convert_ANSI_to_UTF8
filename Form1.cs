1703476db73cde56096faacd78a4b5ce222345b8
Surasak Naka <SurasakNaka@dch-holdings.com>
2019-11-11 18:20:02 +0700

    C

--- Form1.cs
+++ Form1.cs
@@ -45,7 +45,7 @@ namespace Convert_ANSI_to_UTF8
                 {
                     System.IO.Directory.CreateDirectory(sPath_bak);
                 }
-                System.IO.File.Move(sPath_Source + "/" + result_Move, sPath_bak + "/" + DateTime.Now.ToString("yyyyMMddhhmmss", (new System.Globalization.CultureInfo("en-US"))) + "_" + result_Move);
+                System.IO.File.Move(sPath_Source + "/" + result_Move, sPath_bak + "/" + result_Move);
                 #endregion
                 return true;
             }
@@ -54,6 +54,31 @@ namespace Convert_ANSI_to_UTF8
                 return false;
             }
            
+        }
+
+        private bool CopyFile(string sPath_Source, string filepath, string PathInput)
+        {
+            try
+            {
+                #region Move File
+                string result_Move;
+                result_Move = Path.GetFileName(filepath);
+                //string sPath_bak = sPath_BAK + "/" + DateTime.Now.ToString("yyyyMMdd");
+                //bool exists = System.IO.Directory.Exists(sPath_bak);
+                //if (!exists)
+                //{
+                //    System.IO.Directory.CreateDirectory(sPath_bak);
+                //}
+            
+                System.IO.File.Copy(sPath_Source + "/" + result_Move, PathInput + "/" + result_Move);
+                #endregion
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -66,12 +91,28 @@ namespace Convert_ANSI_to_UTF8
             string MailTo = System.Configuration.ConfigurationSettings.AppSettings["MailTo"].ToString();
             string smtp = System.Configuration.ConfigurationSettings.AppSettings["SMTP"].ToString();
             string strSubject = System.Configuration.ConfigurationSettings.AppSettings["sSubjectmail"].ToString();
+            string Pathhsefle = System.Configuration.ConfigurationSettings.AppSettings["Pathhsefle"].ToString();
+
+            string FtpSync_Inbound = System.Configuration.ConfigurationSettings.AppSettings["FtpSync_Inbound"].ToString();
+            string WorkingDirectory = System.Configuration.ConfigurationSettings.AppSettings["WorkingDirectory"].ToString();
+
+            string FtpSync_Bat2 = System.Configuration.ConfigurationSettings.AppSettings["FtpSync_Bat2"].ToString();
+            string WorkingDirectory_Bat2 = System.Configuration.ConfigurationSettings.AppSettings["WorkingDirectory_Bat2"].ToString();
+
 
             ClassLibrarySendMail.ClassLibrarySendMail classmail = new ClassLibrarySendMail.ClassLibrarySendMail();
             string fileName = string.Empty;
             try
             {
-    
+                foreach (string filepaths in Directory.GetFiles(Pathhsefle, "*.hse"))
+                {
+                    bool bResult = false;
+                    fileName = string.Empty;
+                    fileName = filepaths;
+                    bResult = CopyFile(Pathhsefle, filepaths, PathInput);
+
+                }
+
                 foreach (string filepath in Directory.GetFiles(PathInput, "*.hse"))
                 {
                     fileName = string.Empty;
@@ -83,6 +124,24 @@ namespace Convert_ANSI_to_UTF8
 
                     }
                 }
+                #region SFTP
+                System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+                pProcess.StartInfo.WorkingDirectory = WorkingDirectory;
+                pProcess.StartInfo.FileName = FtpSync_Inbound;
+                pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+                pProcess.Start();
+                pProcess.WaitForExit();
+                #endregion
+
+                #region Bat TTN
+                pProcess = new System.Diagnostics.Process();
+                pProcess.StartInfo.WorkingDirectory = WorkingDirectory_Bat2;
+                pProcess.StartInfo.FileName = FtpSync_Bat2;
+                pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+                pProcess.Start();
+                pProcess.WaitForExit();
+                #endregion
+
                 classmail.Sendmail(MailTo, smtp, "Success Convert TTN ANSI to UTF-8", MailFrom, "Success TTN Convert ANSI to UTF-8");
             }
             catch (Exception ex)
